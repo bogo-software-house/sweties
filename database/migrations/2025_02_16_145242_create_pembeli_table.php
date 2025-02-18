3a2bf255407b607f197987dffb62e534f116db81
aqil zaki <132360038+aqilzaki@users.noreply.github.com>
2025-02-18 11:03:51 +0700

    tambahan coloums di table pembeli jo

--- database/migrations/2025_02_16_145242_create_pembeli_table.php
+++ database/migrations/2025_02_16_145242_create_pembeli_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('wa');
             $table->string('alamat');
+            $table->integer('point_pembeli');
             $table->timestamps();
         });
     }
